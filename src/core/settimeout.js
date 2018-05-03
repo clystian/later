b2cf6781c7d310f7e91875867151b9eacd86a062
Alexandre Almeida <alex_til@hotmail.com>
2018-05-03 02:50:09 +0100

    Updated setTimeout and setInterval to use timezones; updated .gitignore to ignore .idea files.

--- src/core/settimeout.js
+++ src/core/settimeout.js
@@ -9,56 +9,128 @@
 * For all details and documentation:
 *     http://github.com/bunkat/later
 */
+const moment = require('moment-timezone');
 
-later.setTimeout = function(fn, sched) {
+later.setTimeout = function(fn, sched, timezone) {
 
-  var s = later.schedule(sched), t;
-  if (fn) {
-    scheduleTimeout();
+  if(timezone) {
+    return setTimeoutWithTimezone(fn, sched, timezone);
+  } else {
+     return setTimeout(fn, sched);
   }
+};
 
-  /**
-  * Schedules the timeout to occur. If the next occurrence is greater than the
-  * max supported delay (2147483647 ms) than we delay for that amount before
-  * attempting to schedule the timeout again.
-  */
-  function scheduleTimeout() {
-    var now = Date.now(),
-        next = s.next(2, now);
-
-    if (!next[0]) {
-      t = undefined;
-      return;
+function setTimeoutWithTimezone(fn, sched, timezone){
+    var s = later.schedule(sched), t;
+    if (fn) {
+        scheduleTimeout();
     }
-
-    var diff = next[0].getTime() - now;
-
-    // minimum time to fire is one second, use next occurrence instead
-    if(diff < 1000) {
-      diff = next[1] ? next[1].getTime() - now : 1000;
-    }
-
-    if(diff < 2147483647) {
-      t = setTimeout(fn, diff);
-    }
-    else {
-      t = setTimeout(scheduleTimeout, 2147483647);
-    }
-  }
-
-  return {
-
-    isDone: function() {
-      return !t;
-    },
 
     /**
-    * Clears the timeout.
-    */
-    clear: function() {
-      clearTimeout(t);
+     * Schedules the timeout to occur. If the next occurrence is greater than the
+     * max supported delay (2147483647 ms) than we delay for that amount before
+     * attempting to schedule the timeout again.
+     */
+    function scheduleTimeout() {
+        var now = getTimezoneDateAsIfItWasUtc(moment.tz(timezone)),
+            next = s.next(2, now)
+                .map(toStringWithoutUTCIndicator);
+
+        if (!next[0]) {
+            t = undefined;
+            return;
+        }
+
+        var diff = next[0].getTime() - now;
+
+        // minimum time to fire is one second, use next occurrence instead
+        if (diff < 1000) {
+            diff = next[1] ? next[1].getTime() - now : 1000;
+        }
+
+        if (diff < 2147483647) {
+            t = setTimeout(fn, diff, timezone);
+        }
+        else {
+            t = setTimeout(scheduleTimeout, 2147483647, timezone);
+        }
     }
 
-  };
+    return {
 
-};
+        isDone: function () {
+            return !t;
+        },
+
+        /**
+         * Clears the timeout.
+         */
+        clear: function () {
+            clearTimeout(t);
+        }
+
+    };
+
+    function toStringWithoutUTCIndicator(date) {
+        const isoString = date.toISOString();
+        return isoString.substring(0, isoString.length - 1);
+    }
+
+    function getTimezoneDateAsIfItWasUtc(timezoneDate) {
+        const formattedAsLocal = timezoneDate.format('YYYY-MM-DDTHH:mm:ss');
+        const formattedAsUTC = formattedAsLocal + 'Z';
+        return new Date(formattedAsUTC);
+    }
+}
+
+function setTimeout(fn, sched){
+    var s = later.schedule(sched), t;
+    if (fn) {
+        scheduleTimeout();
+    }
+
+    /**
+     * Schedules the timeout to occur. If the next occurrence is greater than the
+     * max supported delay (2147483647 ms) than we delay for that amount before
+     * attempting to schedule the timeout again.
+     */
+    function scheduleTimeout() {
+        var now = Date.now(),
+            next = s.next(2, now)
+
+        if (!next[0]) {
+            t = undefined;
+            return;
+        }
+
+        var diff = next[0].getTime() - now;
+
+        // minimum time to fire is one second, use next occurrence instead
+        if (diff < 1000) {
+            diff = next[1] ? next[1].getTime() - now : 1000;
+        }
+
+        if (diff < 2147483647) {
+            t = setTimeout(fn, diff, timezone);
+        }
+        else {
+            t = setTimeout(scheduleTimeout, 2147483647, timezone);
+        }
+    }
+
+    return {
+
+        isDone: function () {
+            return !t;
+        },
+
+        /**
+         * Clears the timeout.
+         */
+        clear: function () {
+            clearTimeout(t);
+        }
+
+    };
+
+}
